1b3c16b722b6d3ff4b03f6aaeadf75e1c18dd007
Kanishka Misra <menogetusername@gmail.com>
2020-12-10 22:02:26 -0500

    missing experiments

--- experiments/run_rosch1975.sh
+++ experiments/run_rosch1975.sh
@@ -3,36 +3,36 @@
 declare -a mlmmodels=("bert-base-uncased" "bert-large-uncased" "roberta-base" "roberta-large" "distilbert-base-uncased" "distilroberta-base" "albert-base-v1" "albert-large-v1" "albert-xlarge-v1" "albert-xxlarge-v1" "google/electra-small-generator" "google/electra-base-generator" "google/electra-large-generator")
 declare -a incrementalmodels=("openai-gpt" "gpt2" "gpt2-medium" "distilgpt2" "gpt2-large" "gpt2-xl")
 
-echo "Running experiments on Masked Language Models"
+# echo "Running experiments on Masked Language Models"
 
-for model in ${mlmmodels[@]}
-do
-    echo "Running experiments for ${model}!"
-    python ../python/sentence_surprisals.py --model ${model} --device cuda:1 --batchsize 50 --dataset ../data/rosch1975/rosch1975.csv
-done
+# for model in ${mlmmodels[@]}
+# do
+#     echo "Running experiments for ${model}!"
+#     python ../python/sentence_surprisals.py --model ${model} --device cuda:1 --batchsize 50 --dataset ../data/rosch1975/rosch1975.csv
+# done
 
 echo "Running experiments on Incremental Language Models"
 
 for model in ${incrementalmodels[@]}
 do
     echo "Running experiments for ${model}!"
-    python ../python/sentence_surprisals.py --model ${model} --device cuda:1 --lmtype incremental --batchsize 50 --dataset ../data/rosch1975.csv
+    python ../python/sentence_surprisals.py --model ${model} --device cuda:1 --lmtype incremental --batchsize 50 --dataset ../data/rosch1975/rosch1975.csv
 done
 
 
-echo "Running experiments on Masked Language Models for Features"
+# echo "Running experiments on Masked Language Models for Features"
 
-for model in ${mlmmodels[@]}
-do
-    echo "Running experiments for ${model}!"
-    python ../python/sentence_surprisals.py --model ${model} --device cuda:1 --batchsize 50 --dataset ../data/rosch1975/rosch1975_features.csv
-done
+# for model in ${mlmmodels[@]}
+# do
+#     echo "Running experiments for ${model}!"
+#     python ../python/sentence_surprisals.py --model ${model} --device cuda:1 --batchsize 50 --dataset ../data/rosch1975/rosch1975_features.csv
+# done
 
-echo "Running experiments on Incremental Language Models for Features"
+# echo "Running experiments on Incremental Language Models for Features"
 
-for model in ${incrementalmodels[@]}
-do
-    echo "Running experiments for ${model}!"
-    python ../python/sentence_surprisals.py --model ${model} --device cuda:1 --lmtype incremental --batchsize 50 --dataset ../data/rosch1975/rosch1975_features.csv
-done
+# for model in ${incrementalmodels[@]}
+# do
+#     echo "Running experiments for ${model}!"
+#     python ../python/sentence_surprisals.py --model ${model} --device cuda:1 --lmtype incremental --batchsize 50 --dataset ../data/rosch1975/rosch1975_features.csv
+# done
 
