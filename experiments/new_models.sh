d7c523b2f914fa3f6d118d994f53c3c24be677b6
Kanishka Misra <menogetusername@gmail.com>
2025-02-15 17:16:42 -0600

    opt

--- experiments/new_models.sh
+++ experiments/new_models.sh
@@ -1,7 +1,7 @@
 # small models, high batch size
-# declare -a models=(gpt2 gpt2-medium gpt2-large EleutherAI/pythia-70m-deduped EleutherAI/pythia-160m-deduped
+# declare -a models=(distilbert/distilgpt2 gpt2 gpt2-medium gpt2-large EleutherAI/pythia-70m-deduped EleutherAI/pythia-160m-deduped
 #     EleutherAI/pythia-410m-deduped)
-declare -a models=(distilbert/distilgpt2)
+declare -a models=(facebook/opt-125m facebook/opt-350m)
 
 for model in "${models[@]}"; do
     echo "Running $model"
@@ -9,9 +9,9 @@ for model in "${models[@]}"; do
 done
 
 # medium models, medium batch size
-# declare -a models=(EleutherAI/pythia-1.4b-deduped EleutherAI/pythia-2.8b-deduped
+# declare -a models=(gpt2-xl EleutherAI/pythia-1.4b-deduped EleutherAI/pythia-2.8b-deduped
 #     EleutherAI/pythia-6.9b-deduped google/Gemma-2-2B allenai/OLMo-2-1124-7B)
-declare -a models=(gpt2-xl)
+declare -a models=(facebook/opt-1.3b facebook/opt-2.7b)
 
 for model in "${models[@]}"; do
     echo "Running $model"
@@ -20,14 +20,16 @@ done
 
 # declare -a models=(EleutherAI/pythia-12b-deduped google/Gemma-2-9B)
 
-# for model in "${models[@]}"; do
-#     echo "Running $model"
-#     python python/newlms.py --model $model --batch-size 16 --device cuda:0
-# done
-
-declare -a models=(allenai/OLMo-2-1124-13B)
+declare -a models=(facebook/opt-6.7b)
 
 for model in "${models[@]}"; do
     echo "Running $model"
-    python python/newlms.py --model $model --batch-size 2 --device cuda:0
+    python python/newlms.py --model $model --batch-size 16 --device cuda:0
 done
+
+# declare -a models=(allenai/OLMo-2-1124-13B)
+
+# for model in "${models[@]}"; do
+#     echo "Running $model"
+#     python python/newlms.py --model $model --batch-size 2 --device cuda:0
+# done
